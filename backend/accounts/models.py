e22a189d30d3fd3745ba91661cdd6c30b0ce5efe
Nigel Yang <nigelyang611@hotmail.com>
2023-02-18 14:42:29 -0500

    Updated models, serializers, urls, views

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, \
+    BaseUserManager
+from django_countries.fields import CountryField
+
 
 # Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, email, password, **other_fields):
+
+        if not email:
+            raise ValueError('Please provide a valid email address')
+
+        email = self.normalize_email(email)
+        user = self.model(email=email, **other_fields)
+        user.set_password(password)
+        user.save()
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(max_length=150, unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    avatar = models.ImageField(upload_to='user_avatars/', blank=True,
+                               null=True)
+    city = models.CharField(max_length=50)
+    country = models.CharField(max_length=50)
+    age = models.IntegerField(max_length=3)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.email
+
+
+class Student(User):
+    grade = models.IntegerField(max_length=2)
+    school = models.CharField(max_length=150)
+    homeroom_id = models.CharField(max_length=150)
+
+
+class Teacher(User):
+    school = models.CharField(max_length=150)
+
+
+class homeroom_id(AbstractBaseUser, PermissionsMixin):
+    homeroom_id = models.CharField(max_length=150)
+    teacher_id = models.CharField(max_length=150)
---
+++ backend/accounts/serializers.py
@@ -0,0 +1,77 @@
+from rest_framework import serializers
+from models import User, Student, Teacher
+
+
+class UserSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'avatar', 'city',
+                  'country', 'age')
+        lookup_field = 'email'
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        try:
+            user.set_password(validated_data['password'])
+            user.save()
+        except KeyError:
+            pass
+        return user
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'avatar', 'city',
+                  'country', 'age', 'grade', 'school', 'homeroom_id')
+        lookup_field = 'email'
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        try:
+            user.set_password(validated_data['password'])
+            user.save()
+        except KeyError:
+            pass
+        return user
+
+
+class TeacherSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('email', 'first_name', 'last_name', 'avatar', 'city',
+                  'country', 'age', 'school')
+        lookup_field = 'email'
+
+    def create(self, validated_data):
+        user = super().create(validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        user = super().update(instance, validated_data)
+        try:
+            user.set_password(validated_data['password'])
+            user.save()
+        except KeyError:
+            pass
+        return user
