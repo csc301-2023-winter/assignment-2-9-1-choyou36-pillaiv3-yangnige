57727655cc4aa16cd02ae9c374b4d0611437b4d5
Youngjun Cho <youngjun.cho@mail.utoronto.ca>
2023-02-19 16:03:10 -0500

    Setup for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -137,3 +137,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Temploary for deployment
+ALLOWED_HOSTS = ['*']
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_CREDENTIALS = True
